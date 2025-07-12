591b0b0b87514ee0b668d89b9aa9fbd3803a6990
javier <jjavierl.bo@gmail.com>
2025-07-11 21:08:24 -0400

    sentencias if

---
+++ sentenciaIf.py
@@ -0,0 +1,23 @@
+
+
+dia = int(input("Ingrese un numero:"))
+#print(type(dia))
+
+if dia == 1 and dia == 'lunes':
+    print("Es Lunes")
+elif dia == 2:
+    print("Es Martes")
+elif dia == 3:
+    print("Es Miercoles")
+elif dia == 4:
+    print("Es Jueves")
+elif dia == 5:
+    print("Es Viernes")
+elif dia == 6:
+    print("Es sabado")
+elif dia == 7:
+    print("domingo")
+else:
+    print("Error")
+
+print("termino el proceso")
---
+++ sentenciaWhile.py
@@ -0,0 +1,14 @@
+contador = 11
+while contador > 1:
+    contador = contador - 1
+    print(contador, end=" ")
+
+
+""" suma=0
+n=int(input("Número positivo:"))
+while n!=0:
+    digito=n%10
+    suma+=digito
+    n=n//10
+print("Suma de los dígitos:", suma) """
+
