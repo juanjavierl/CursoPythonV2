5f14f57c2687431b7d82bf3e6818aaf49774cb09
javier <jjavierl.bo@gmail.com>
2025-08-11 21:14:49 -0400

    Herencia de clases en Python

--- modulo2/poo/clases.py
+++ modulo2/poo/clases.py
@@ -1,16 +1,70 @@
-class Auto:
-    color = ""
-    ruedas = 0
+import json
+import random
+import math
 
-    def crear_coche(self, ruedas_auto, color_auto):
+class Auto:
+    def __init__(self, ruedas_auto, color_auto, placa):
         self.color = color_auto
         self.ruedas = ruedas_auto
-        return f"Auto color: {self.color} ruedas: {self.ruedas}"
+        self.__placa = placa
+        
+    def __getattr__(self):
+        return self.ruedas
+    
+    def __str__(self):
+        return f"Auto con: {self.ruedas} ruedas y es de color {self.color}"
 
-objeto = Auto()
-print(objeto.crear_coche(4, "Rojo"))
+    def __setattr__(self, color, newColor):
+        super().__setattr__(color, newColor)
 
-objeto1 = Auto()
+    def datos_json(self):
+        datos = {'ruedas ':self.ruedas,'color ':self.color}
+        return json.dumps(datos)
+    
+    def mostrar_placa(self):
+        return self.__placa
+    
+    def __calentar(sel): 
+        return "El motor se está calentando"
+    
+    def encender(self):
+        return self.__calentar()
 
-print(objeto1.crear_coche(6, "verde"))
-print(objeto1.color)
+class AutoElectrico(Auto):
+    def __init__(self, ruedas_auto, color_auto, placa, energia):
+        super().__init__(ruedas_auto, color_auto, placa)
+        self.energia = energia
+
+autoElec = AutoElectrico(4,"Azul", "xyz-321", "Alto")
+print(autoElec.mostrar_placa())
+
+
+
+class Persona:
+    def __init__(self, nombre, apellido, ci):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.__ci = ci
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} Apellido {self.apellido}"
+    
+    def conducir(self, auto):
+        return auto.encender()
+    
+    def mostrar_color(self, auto):
+        return auto.color
+
+# auto1 = Auto(8, "Blanco", "ABC-123")
+# print(auto1)
+
+# persona = Persona("Jose", "Martinez", 12345)
+# print("Llamada al metodo del auto: ", persona.conducir(auto1))
+# print(f"La persona: {persona.nombre} tiene el auto color: {persona.mostrar_color(auto1)}")
+
+
+# auto1.ruedas = 4
+# print(auto1)
+# print(auto1.datos_json())
+
+# print("Placa: ", auto1.encender())
